d145204238eb7ce640e27ad8cd94bb61787da7f6
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-07 10:27:23 +0500

    added files

---
+++ Basics of datatypes.js
@@ -0,0 +1,45 @@
+
+//js basics first quizes
+let a="hello";
+let b=345;
+console.log(a+ b);
+
+//adding type of 
+console.log(typeof(a+b));
+
+console.log("creating const object and changing it lateron");
+//adding a key in object 
+const a1={
+  name:"harry",
+  age:34,
+isPrinciple:true
+}
+a1['friend']="Harry";
+
+console.log(a1);
+
+
+
+//switch statement
+let age=prompt("whats your age")
+switch(age){
+  case 12:
+    console.log("your age is 12")
+    break;
+    case 13:
+    console.log("your age is 13")
+    break;
+    case 14:
+    console.log("your age is 14")
+    break;
+  default:console.log(
+    "your age is not speacil ")
+}
+
+//retun array first value
+const Numb=[1,2,3,4,5,6,7,8];
+console.log(Numb[5]);
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,12 @@
+//loops
+
+for(var i=1; i<=10; i++)
+  console.log("the value of loop is " + i)
+
+
+function oddlop(){
+  for(i=3; i<30; i++){
+    2(i+1);
+  }
+console.log("the valye of odd numbers loop is" + oddlop())
+    }
