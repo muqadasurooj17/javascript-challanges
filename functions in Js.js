05a3f12fb869664d7bc54037d1e422f5f49a1b6f
MuqadasUrooj <muqadasurooj123@gmail.com>
2023-09-07 11:30:31 +0500

    average function

--- functions in Js.js
+++ functions in Js.js
@@ -37,3 +37,27 @@ function myAge(age){
 console.log("the age of" +age +" years human according to days is " + myAge(age));
 
 
+// Function to calculate the average of four digits
+function calculateAverage() {
+    // Prompt the user to enter four digits and store them in variables
+    var num1 = parseFloat(prompt("Enter the first digit:"));
+    var num2 = parseFloat(prompt("Enter the second digit:"));
+    var num3 = parseFloat(prompt("Enter the third digit:"));
+    var num4 = parseFloat(prompt("Enter the fourth digit:"));
+  
+    // Check if any of the inputs is not a valid number
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || isNaN(num4)) {
+      alert("Please enter valid numeric values for all four digits.");
+      return;
+    }
+  
+    // Calculate the average
+    var average = (num1 + num2 + num3 + num4) / 4;
+  
+    // Display the result
+    console.log("The average of the four digits is: " + average);
+  }
+  
+  // Call the function to calculate the average
+  calculateAverage();
+  
